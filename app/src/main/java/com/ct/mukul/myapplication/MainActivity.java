abd3eb273e19d75058a5aa4bef185e2e55e58f55
mahbubmukul <mahbub.mukul@gmail.com>
2019-02-10 20:10:31 +0600

    button click

--- app/src/main/java/com/ct/mukul/myapplication/MainActivity.java
+++ app/src/main/java/com/ct/mukul/myapplication/MainActivity.java
@@ -15,27 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(i);
-                finish();
-            }
-        }, 5000);
-
-
-
-//        Button btn = findViewById(R.id.button);
-//        btn.setOnClickListener(new View.OnClickListener() {
+//        new Handler().postDelayed(new Runnable() {
 //            @Override
-//            public void onClick(View v) {
-////                Toast.makeText(MainActivity.this, "Clicked..",
-////                        Toast.LENGTH_LONG).show();
-//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-//                startActivity(intent);
+//            public void run() {
+//                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+//                startActivity(i);
 //                finish();
 //            }
-//        });
+//        }, 5000);
+
+
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Clicked..",
+//                        Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }

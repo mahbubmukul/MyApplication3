e4b676164039a716fb87b8c38cd1ded946bc161e
mahbubmukul <mahbub.mukul@gmail.com>
2019-02-10 20:06:15 +0600

    commit

---
+++ app/src/main/java/com/ct/mukul/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ct.mukul.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+
+
+
+//        Button btn = findViewById(R.id.button);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                Toast.makeText(MainActivity.this, "Clicked..",
+////                        Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+    }
+}
